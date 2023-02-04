76791e2cddedac6d9483fd2ff84f14bb38a6dfb7
katievlasic <118415802+katievlasic@users.noreply.github.com>
2023-02-04 17:55:26 -0600

    continue questions population

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // TODO: Include packages needed for this application
+npm i inquirer@8.2.4
 npm install inquirer
 
 var inquirer = require('inquirer');
@@ -25,15 +26,19 @@ inquirer.createPromptModule()
 const questions = [
     {
         type: 'input',
+        name: 'title',
         message: "What is the title of your project?",
 },
 {
     type: 'input',
+    name: 'description',
     message: "Please enter a description of the project.",
 },
 {
-    type: 'input',
+    type: 'checkbox',
+    name: 'tableOfContents',
     message: "Please enter a table of contents list.",
+    choices: ['Description','License','Contributors'],
 },
 {
     type: 'input',
@@ -44,8 +49,10 @@ const questions = [
     message: "Usage?",
 },
 {
-    type: 'input',
+    type: 'list',
+    name: 'license',
     message: "What license (if any) are you using?",
+    choices: ['none','Apache LIcense 2.0','GNU General Pulic License v3.0','MIT License','BSD 2-Clause "Simplified" License',"BSD 3-Clause New or Revised License","Boost Software License 1.0","Creative Commons Zero v1.0 Universal","Eclipse Public License 2.0","GNU Affero General Public License v3.0","GNU General Public License v2.0","GNU Lesser General Public License v2.1","Mozilla Public License 2.0", "The Unlicense","other"],
 },
 {
     type: 'input',
