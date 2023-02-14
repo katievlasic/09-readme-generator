fcad5b8ceea5622d03eb501727310d341d9854bb
katievlasic <118415802+katievlasic@users.noreply.github.com>
2023-02-13 21:16:20 -0600

    image link rendering in the README generated

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -1,19 +1,19 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+function generateMarkdown(data) {
+// License badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license = "MIT License") {
-    return `[MIT License]("https://img.shields.io/badge/license-MIT-brightgreen")`;
+    return `![MIT License](https://img.shields.io/badge/license-MIT-brightgreen)`;
   } else if (license = "Apache License 2.0") {
-    return `[Apache License 2.0](https://img.shields.io/badge/license-Apache--2.0-brightgreen")`;
+    return `![Apache License 2.0](https://img.shields.io/badge/license-Apache--2.0-brightgreen")`;
   } else if (license = "GNU General Pulic License v3.0"){
-    return  `[Apache License 2.0](https://img.shields.io/badge/license-GNU%20v3.0-brightgreen")`;
+    return  `![GNU License](https://img.shields.io/badge/license-GNU%20v3.0-brightgreen")`;
   } else {
     ""
   }
 };
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// License link. If no license, return an empty string
 function renderLicenseLink(license) {
   if (license = "MIT License") {
     return `[MIT License]("https://en.wikipedia.org/wiki/MIT_License")`;
@@ -26,8 +26,7 @@ function renderLicenseLink(license) {
   }
 };
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// License section of README. If no license, return an empty string
 function renderLicenseSection(license) {
   if (license = "MIT License") {
     return `"A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code."`;
@@ -41,19 +40,17 @@ function renderLicenseSection(license) {
 
 };
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+// Generate .md for README
 const license = `${data.license}`
 renderLicenseBadge(license);
 renderLicenseLink(license);
 renderLicenseSection(license);
-return `# ${data.title} \n \n ## Description \n ${data.description} \n \n ## Installation \n ${data.install} \n \n ## Usage \n ${data.use} \n \n ## Credits \n ${data.collab} \n \n ## Tests \n ${data.test} \n \n ## Questions? \n ${data.quest} \n \n ## License \n ${data.license} \n`;
+return `# ${data.title} \n \n ## Description \n ${data.description} \n \n ## Installation \n ${data.install} \n \n ## Usage \n ${data.use} \n \n ## Credits \n ${data.collab} \n \n ## Tests \n ${data.test} \n \n ## Questions? \n ${data.quest} \n \n ## License \n ${data.license} \n \n ${renderLicenseBadge(license)} \n \n ${renderLicenseLink(license)} \n \n ${renderLicenseSection(license)}`;
 
 };
 
 // module exports allows export from one file to another
 module.exports = generateMarkdown;
-//module.exports = renderLicenseBadge;
 
 // ` = template literal : literal translation, no JS reading 
 // $ = template expression to access dynamically JS
--- dist/README.md
+++ dist/README.md
@@ -10,13 +10,19 @@
  4 
  
  ## Credits 
- 1 
+ 4 
  
  ## Tests 
- 2 
+  
  
  ## Questions? 
   
  
  ## License 
  GNU General Pulic License v3.0 
+ 
+ ![MIT License](https://img.shields.io/badge/license-MIT-brightgreen) 
+ 
+ [MIT License]("https://en.wikipedia.org/wiki/MIT_License") 
+ 
+ "A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code."
